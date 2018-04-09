d6d8c23f60e738a9612a98cf4d675ff595da3dab
Juan David Villate <jdvillate@hotmail.com>
2018-04-09 16:18:01 -0500

    02

---
+++ MySql/MVCMySql/WebApplication2/Controllers/usuariosController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClassLibrary1;
+
+namespace WebApplication2.Controllers
+{
+    public class usuariosController : Controller
+    {
+        private ejemplo_usuarioEntities db = new ejemplo_usuarioEntities();
+
+        // GET: usuarios
+        public async Task<ActionResult> Index()
+        {
+            var usuarios = db.usuarios.Include(u => u.tipousuario);
+            return View(await usuarios.ToListAsync());
+        }
+
+        // GET: usuarios/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuario usuario = await db.usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // GET: usuarios/Create
+        public ActionResult Create()
+        {
+            ViewBag.idTipo = new SelectList(db.tipousuarios, "idTipoUsuario", "nombre");
+            return View();
+        }
+
+        // POST: usuarios/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "idUsuario,idTipo,nombre,apellido,telefono,correo")] usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.usuarios.Add(usuario);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idTipo = new SelectList(db.tipousuarios, "idTipoUsuario", "nombre", usuario.idTipo);
+            return View(usuario);
+        }
+
+        // GET: usuarios/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuario usuario = await db.usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idTipo = new SelectList(db.tipousuarios, "idTipoUsuario", "nombre", usuario.idTipo);
+            return View(usuario);
+        }
+
+        // POST: usuarios/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "idUsuario,idTipo,nombre,apellido,telefono,correo")] usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idTipo = new SelectList(db.tipousuarios, "idTipoUsuario", "nombre", usuario.idTipo);
+            return View(usuario);
+        }
+
+        // GET: usuarios/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuario usuario = await db.usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            usuario usuario = await db.usuarios.FindAsync(id);
+            db.usuarios.Remove(usuario);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
